37515d97e4595a6cced271f22d3ea3a42bbe69d3
Artur Paschoal <128155308+paschoal-artur@users.noreply.github.com>
2023-03-31 11:15:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if age < 0 :
     print("impossível!")
     print("não precisa se alistar.")
 elif age < 18 :
-    print("não precisa se alistar")
+    print("não precisa se alistar.")
 elif age > 18 and age < 65 :
     print("Não esqueça de votar na próxima eleição.")
 elif age > 65 :
